38fe893fd980a78f67b83a47d990b90e9444a953
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-02 02:43:08 +0700

    feat[WIP]: update register route functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,38 @@
-import fastify from 'fastify'
+import "make-promises-safe"
+import dotenv from "dotenv"
+import fastify, { FastifyInstance } from "fastify"
+
+dotenv.config({
+  path: `.env`,
+})
+
+import knex from "./helpers/init-db"
+import authenticate from "./routes/authenticate"
 
 const server = fastify({
-  logger: true
+  logger: true,
 })
 
-server.get('/', (_req, res) => {
-  res.send({status: "ok!"})
+server.decorate("knex", knex)
+
+server.register(authenticate)
+
+server.setErrorHandler(async (error, _req, reply) => {
+  if (error.validation) {
+    reply.status(400).send({
+      statusCode: 400,
+      error: "Bad Request",
+    })
+  }
 })
 
-server.listen(3000)
+async function startServer(server: FastifyInstance) {
+  try {
+    await server.listen(3000)
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+startServer(server)

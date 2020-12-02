46fd0b5a3436b9dd00c09c7de3400acb5fe4e93c
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-02 12:20:21 +0700

    feat: add /register route test

---
+++ backend/src/__tests__/index.test.ts
@@ -0,0 +1,60 @@
+import request from "supertest"
+import mockKnex from "mock-knex"
+
+import fastify from "../app"
+import db from "../helpers/init-db"
+
+jest.mock("../helpers/generate-password")
+
+beforeAll(async () => {
+  await fastify.ready()
+})
+
+beforeEach(() => {
+  mockKnex.mock(db)
+})
+
+afterEach(() => {
+  mockKnex.unmock(db)
+})
+
+afterAll(async () => {
+  await fastify.close()
+})
+
+describe("app index", () => {
+  it("should query to /register and recieved 200 success with expected response field", async done => {
+    request(fastify.server)
+      .post("/register")
+      .send({ username: "john" })
+      .set("Accept", "application/json")
+      .expect(200)
+      .expect({
+        statusCode: 200,
+        message: "Register Success",
+        password: "xxxxxx",
+      })
+      .end(done)
+  })
+
+  it("should query to /register and recieved 406 when send an exist name to a server", async done => {
+    const dbTracker = mockKnex.getTracker()
+
+    dbTracker.install()
+
+    dbTracker.on("query", query => {
+      query.reject("Key (username)=(john) is already exist")
+    })
+
+    request(fastify.server)
+      .post("/register")
+      .send({ username: "john" })
+      .set("Accept", "application/json")
+      .expect(406)
+      .expect({
+        statusCode: 406,
+        error: "Database Query Not Accept"
+      })
+      .end(done)
+  })
+})

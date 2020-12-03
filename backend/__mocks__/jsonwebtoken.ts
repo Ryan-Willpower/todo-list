d374b6603e626cfb9dd85739e06a6ddabc6950f1
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-04 02:28:56 +0700

    chore: add verify and JsonWebTokenError mock

--- backend/__mocks__/jsonwebtoken.ts
+++ backend/__mocks__/jsonwebtoken.ts
@@ -1,1 +1,9 @@
 export const sign = jest.fn().mockReturnValue("aaaa.bbbb.cccc")
+export const verify = jest.fn().mockReturnValue({
+  author: "john",
+  type: "access",
+  iat: Date.now(),
+  exp: Date.now(),
+})
+export const JsonWebTokenError = jest.requireActual("jsonwebtoken")
+  .JsonWebTokenError

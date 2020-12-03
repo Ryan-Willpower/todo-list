fd6224281132d86eaa16859e6a62e513117738cd
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-04 01:49:35 +0700

    feat: add interface and enum for graphql

---
+++ backend/src/@types/graphql/index.ts
@@ -0,0 +1,54 @@
+export interface IAddCardParams {
+  content: string
+  category?: string
+}
+
+export interface IUpdateCardParams {
+  id: string
+  content?: string
+  category?: string
+  status?: CARD_STATUS
+}
+
+export interface IDeleteCardParams {
+  id: string
+}
+
+export interface IUpdateCardStatusParams {
+  id: string
+  status: CARD_STATUS
+}
+
+export interface IAppContext {
+  id: string
+  author: string
+}
+
+export enum CARD_STATUS {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
+
+export interface ICard {
+  id: string
+  author: string
+  status: CARD_STATUS
+  content: string
+  category?: string | null
+  iat: Date | string
+}
+
+export interface ICategory {
+  id: string
+  name: string
+}
+
+export enum QUERY_STATUS {
+  SUCCESS = "SUCCESS",
+  FAIL = "FAIL",
+}
+
+export interface QueryStatus {
+  status: QUERY_STATUS
+  message?: string
+}

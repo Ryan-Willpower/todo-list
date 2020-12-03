6b0f48ef046a9164d6eb520fca5c618b02d77c10
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-04 01:52:19 +0700

    feat: add updateCard resolver

---
+++ backend/src/__tests__/graphql/resolvers/cards/update.test.ts
@@ -0,0 +1,100 @@
+import mockKnex from "mock-knex"
+import {
+  CARD_STATUS,
+  IAppContext,
+  ICard,
+  ICategory,
+} from "../../../../@types/graphql"
+
+import db from "../../../../helpers/init-db"
+import { updateCard } from "../../../../resolvers/cards/mutations/update"
+
+const dbTracker = mockKnex.getTracker()
+
+beforeEach(() => {
+  mockKnex.mock(db)
+  dbTracker.install()
+})
+
+afterEach(() => {
+  dbTracker.uninstall()
+  mockKnex.unmock(db)
+})
+
+const context: IAppContext = {
+  id: "someid",
+  author: "john",
+}
+
+const date = Date.now()
+
+const dbResponse: { categories: ICategory; cards: ICard } = {
+  categories: {
+    id: "xxxx",
+    name: "test",
+  },
+  cards: {
+    id: "yyyy",
+    author: context.id,
+    content: "Hello world",
+    status: CARD_STATUS.ACTIVE,
+    category: null,
+    iat: date.toExponential(),
+  },
+}
+
+describe("graphql/mutation/updateCard", () => {
+  it("should update content", async () => {
+    dbTracker.on("query", query => {
+      query.response([dbResponse.cards])
+    })
+
+    const updateCardResponse = await updateCard(
+      null,
+      {
+        id: "yyyy",
+        content: "Hello world",
+      },
+      context
+    )
+
+    expect(updateCardResponse).toStrictEqual(dbResponse.cards)
+  })
+
+  it("should update status", async () => {
+    dbTracker.on("query", query => {
+      query.response([dbResponse.cards])
+    })
+
+    const updateCardResponse = await updateCard(
+      null,
+      {
+        id: "yyyy",
+        status: CARD_STATUS.ACTIVE,
+      },
+      context
+    )
+
+    expect(updateCardResponse).toStrictEqual(dbResponse.cards)
+  })
+
+  it("should update status", async () => {
+    dbTracker.on("query", (query, step) => {
+      ;[
+        () => query.response([dbResponse.categories]),
+        () => query.response([dbResponse.cards]),
+      ][step - 1]()
+    })
+
+    const updateCardResponse = await updateCard(
+      null,
+      {
+        id: "yyyy",
+        category: "test",
+      },
+      context
+    )
+
+    expect(updateCardResponse).toStrictEqual(dbResponse.cards)
+  })
+})
---
+++ backend/src/resolvers/cards/mutations/update.ts
@@ -0,0 +1,49 @@
+import knex from "../../../helpers/init-db"
+import {
+  IUpdateCardParams,
+  IAppContext,
+  ICard,
+  ICategory,
+} from "../../../@types/graphql"
+
+export const updateCard = async (
+  _: any,
+  args: IUpdateCardParams,
+  context: IAppContext
+): Promise<ICard> => {
+  const updateQuery: IUpdateCardParams = {
+    id: args.id,
+  }
+
+  if (args.content) {
+    updateQuery.content = args.content
+  }
+
+  if (args.status) {
+    updateQuery.status = args.status
+  }
+
+  if (args.category) {
+    const categoryResponse = await knex<ICategory>("categories")
+      .insert({
+        name: args.category,
+      })
+      .onConflict("name")
+      .merge({
+        name: args.category,
+      })
+      .returning(["id", "name"])
+
+    updateQuery.category = categoryResponse[0].id
+  }
+
+  const cardResponse = await knex<ICard>("cards")
+    .update(updateQuery)
+    .where({
+      id: args.id,
+      author: context.id,
+    })
+    .returning("*")
+
+  return cardResponse[0]
+}

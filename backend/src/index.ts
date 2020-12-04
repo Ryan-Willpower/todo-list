b5735c73f06e8088d9e5c20f9ec7edc510af0080
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-05 00:37:18 +0700

    chore: accept production environment variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,10 @@ import server from "./app"
 
 export async function startServer(server: FastifyInstance) {
   try {
-    await server.listen(3000)
+    const port = process.env.PORT || 3000
+    const hostname = process.env.HOSTNAME || "localhost"
+
+    await server.listen(port, hostname)
   } catch (err) {
     server.log.error(err)
     process.exit(1)

b13232bcaab8dbf7801294a2b1abfc35273367ab
Ryan-Willpower <surasak.c.work@gmail.com>
2020-12-07 19:30:32 +0700

    fix: add env configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,8 @@
 // next.config.js
 const withSass = require("@zeit/next-sass")
 module.exports = withSass({
-  /* config options here */
+  env: {
+    BACKEND_HOSTNAME: "http://localhost:8000",
+    GRAPHQL_HOSTNAME: "http://localhost:8000/graphql",
+  },
 })
